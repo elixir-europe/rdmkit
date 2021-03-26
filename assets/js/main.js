dee819f9506fd8b01b25ce90d0e842ac63182cfb
bedroesb <bedro@psb.vib-ugent.be>
2021-03-26 14:05:18 +0100

    no cookie banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,39 +56,4 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 })
 })
 
-/**
- * Cookie consent
- */
-
-function setCookie(cookieName, cookieValue, numdaystilexpireasinteger) {
-  var d = new Date();
-  d.setTime(d.getTime() + (numdaystilexpireasinteger*24*60*60*1000));
-  var expires = "expires="+ d.toUTCString();
-  document.cookie = cookieName+ "=" + cookieValue + ";" + expires + ";path=/";
-}
-
-function getCookie(cookieName) {
-  var name = cookieName+ "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-          c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-          return c.substring(name.length, c.length);
-      }
-  }
-  return "";
-}
-
-function showLaw() {
-  var x = getCookie("cookieName");  //call cookie to get its value
-  if (x != "") {
-      $("#lawmsg").remove();
-  } else {
-          setCookie("cookieName", "cookieValue", 2);
-      }
-  }
 
