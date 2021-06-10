b9bb9c46ff5874a07cb4f8c8ed9445555a8e306f
bedroesb <bedro@psb.vib-ugent.be>
2021-06-10 23:23:00 +0200

    animated numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,6 +74,42 @@ jQuery(function ($) {
 });
 
 
+/**
+ * Animate numbers when in viewport
+ */
+
+// inViewport jQuery plugin
+// https://stackoverflow.com/a/26831113/383904
+$(function($, win) {
+  $.fn.inViewport = function(cb) {
+    return this.each(function(i,el){
+      function visPx(){
+        var H = $(this).height(),
+            r = el.getBoundingClientRect(), t=r.top, b=r.bottom;
+        return cb.call(el, Math.max(0, t>0? H-t : (b<H?b:H)));  
+      } visPx();
+      $(win).on("resize scroll", visPx);
+    });
+  };
+}(jQuery, window));
+
+
+jQuery(function($) { // DOM ready and $ in scope
+  $(".count-number").inViewport(function(px) { // Make use of the `px` argument!!!
+    if(px>0 && !this.initNumAnim) { 
+      this.initNumAnim = true; // Set flag to true to prevent re-running the same animation
+      $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+      }, {
+        duration: 1000,
+        step: function (now) {
+          $(this).text(Math.ceil(now));
+        }
+      });         
+    }
+  });
+
+});
 
 /**
  * Search based on just the docs
