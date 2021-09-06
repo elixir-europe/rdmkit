e5be1cbeb8139855bb66d6c9ed847d920ccb3177
bedroesb <bedro@psb.vib-ugent.be>
2021-09-06 16:24:25 +0200

    start

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,6 +62,17 @@ $(function () {
 })
 
 /**
+ * Activate popovers
+ */
+
+ $(function () {
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+})
+
+/**
  * Toggle button texts
  */
 jQuery(function ($) {
