b919d96f171d8d2832ed963417ccedbc6ab5f7d1
bedroesb <bedro@psb.vib-ugent.be>
2021-11-19 18:33:30 +0100

    search bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,45 +7,45 @@ permalink: assets/js/main.js
  * AnchorJS
  */
 $(document).ready(function () {
-  anchors.options = {
-    placement: 'left'
-  };
-  anchors.add('h2:not(.no-anchor)');
+    anchors.options = {
+        placement: 'left'
+    };
+    anchors.add('h2:not(.no-anchor)');
 })
 
 /**
  * Function to open external links in separate tab
  */
 $(document).ready(function external_new_window() {
-  for (var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) {
-    var b = c[a];
-    if (b.getAttribute("href") && b.hostname !== location.hostname) {
-      b.target = "_blank";
-      b.rel = "noopener";
+    for (var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) {
+        var b = c[a];
+        if (b.getAttribute("href") && b.hostname !== location.hostname) {
+            b.target = "_blank";
+            b.rel = "noopener";
+        }
     }
-  }
 });
 
 /**
  * Settings for side navigation
  */
 $(document).ready(function () {
-  // Initialize navgoco with default options
-  $("#menu").navgoco({
-    caretHtml: '',
-    accordion: true,
-    openClass: 'active', // open
-    save: false, // leave false or nav highlighting doesn't work right
-    cookie: {
-      name: 'navgoco',
-      expires: false,
-      path: '/'
-    },
-    slide: {
-      duration: 400,
-      easing: 'swing'
-    }
-  });
+    // Initialize navgoco with default options
+    $("#menu").navgoco({
+        caretHtml: '',
+        accordion: true,
+        openClass: 'active', // open
+        save: false, // leave false or nav highlighting doesn't work right
+        cookie: {
+            name: 'navgoco',
+            expires: false,
+            path: '/'
+        },
+        slide: {
+            duration: 400,
+            easing: 'swing'
+        }
+    });
 });
 
 /**
@@ -53,12 +53,12 @@ $(document).ready(function () {
  */
 
 $(function () {
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl, {
-      animation: false
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl, {
+            animation: false
+        })
     })
-  })
 })
 
 /**
@@ -66,22 +66,22 @@ $(function () {
  */
 
 $(function () {
-  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-  })
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
 })
 
 /**
  * Toggle button texts
  */
 jQuery(function ($) {
-  $('.toggle-text[data-bs-toggle="collapse"]').on('click', function () {
-    $(this)
-      .data('text-original', $(this).text())
-      .text($(this).data('text-alt'))
-      .data('text-alt', $(this).data('text-original'));
-  });
+    $('.toggle-text[data-bs-toggle="collapse"]').on('click', function () {
+        $(this)
+            .data('text-original', $(this).text())
+            .text($(this).data('text-alt'))
+            .data('text-alt', $(this).data('text-original'));
+    });
 });
 
 
@@ -89,31 +89,31 @@ jQuery(function ($) {
  * Making relevant events visible
  */
 function nowToDateString() {
-  return new Date().toISOString().substring(0, 10);
+    return new Date().toISOString().substring(0, 10);
 };
 
 function showUpcomingEvents() {
-  var dstr = nowToDateString();
-  var events_block = $(document.getElementsByClassName("upcoming-events"));
-  var elements = $('li.upcoming-event').filter(function () {
-    return $(this).data('start') >= dstr;
-  });
-  if ($(elements).length > 0) {
-    events_block.show()
-  }
-  elements.show();
+    var dstr = nowToDateString();
+    var events_block = $(document.getElementsByClassName("upcoming-events"));
+    var elements = $('li.upcoming-event').filter(function () {
+        return $(this).data('start') >= dstr;
+    });
+    if ($(elements).length > 0) {
+        events_block.show()
+    }
+    elements.show();
 };
 
 function showPastEvents() {
-  var dstr = nowToDateString();
-  var events_block = $(document.getElementsByClassName("past-events"));
-  var elements = $('li.past-event').filter(function () {
-    return $(this).data('start') < dstr;
-  });
-  if ($(elements).length > 0) {
-    events_block.show()
-  }
-  elements.show();
+    var dstr = nowToDateString();
+    var events_block = $(document.getElementsByClassName("past-events"));
+    var elements = $('li.past-event').filter(function () {
+        return $(this).data('start') < dstr;
+    });
+    if ($(elements).length > 0) {
+        events_block.show()
+    }
+    elements.show();
 };
 
 
@@ -125,33 +125,33 @@ function showPastEvents() {
 // inViewport jQuery plugin
 // https://stackoverflow.com/a/26831113/383904
 $(function ($, win) {
-  $.fn.inViewport = function (cb) {
-    return this.each(function (i, el) {
-      function visPx() {
-        var H = $(this).height(),
-          r = el.getBoundingClientRect(), t = r.top, b = r.bottom;
-        return cb.call(el, Math.max(0, t > 0 ? H - t : (b < H ? b : H)));
-      } visPx();
-      $(win).on("resize scroll", visPx);
-    });
-  };
+    $.fn.inViewport = function (cb) {
+        return this.each(function (i, el) {
+            function visPx() {
+                var H = $(this).height(),
+                    r = el.getBoundingClientRect(), t = r.top, b = r.bottom;
+                return cb.call(el, Math.max(0, t > 0 ? H - t : (b < H ? b : H)));
+            } visPx();
+            $(win).on("resize scroll", visPx);
+        });
+    };
 }(jQuery, window));
 
 
 jQuery(function ($) { // DOM ready and $ in scope
-  $(".count-number").inViewport(function (px) { // Make use of the `px` argument!!!
-    if (px > 0 && !this.initNumAnim) {
-      this.initNumAnim = true; // Set flag to true to prevent re-running the same animation
-      $(this).prop('Counter', 0).animate({
-        Counter: $(this).text()
-      }, {
-        duration: 1000,
-        step: function (now) {
-          $(this).text(Math.ceil(now));
+    $(".count-number").inViewport(function (px) { // Make use of the `px` argument!!!
+        if (px > 0 && !this.initNumAnim) {
+            this.initNumAnim = true; // Set flag to true to prevent re-running the same animation
+            $(this).prop('Counter', 0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 1000,
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
         }
-      });
-    }
-  });
+    });
 
 });
 
@@ -159,27 +159,47 @@ jQuery(function ($) { // DOM ready and $ in scope
  * Filter cards by affiliation
  */
 
- $(document).ready(function () {
-  document
-    .getElementById("affiliations-input")
-    .addEventListener("change", function () {
-      const cols = document.querySelectorAll(".col");
-      for (col of cols) {
-        const colCategories = col.getAttribute("data-affiliations").split(" ");
-        if (colCategories.indexOf(this.value) != -1 || this.value === "reset") {
-          col.style.display = "block";
-        } else {
-          col.style.display = "none";
+$(document).ready(function () {
+    document
+        .getElementById("affiliations-input")
+        .addEventListener("change", function () {
+            document.getElementById('title-search').value = ''
+            const cols = document.querySelectorAll(".navigation-tiles .col");
+            for (col of cols) {
+                const colCategories = col.getAttribute("data-affiliations").split(" ");
+                if (colCategories.indexOf(this.value) != -1 || this.value === "reset") {
+                    col.classList.remove("d-none");
+                } else {
+                    col.classList.add("d-none");
+                }
+            }
+        });
+    document.getElementById("clearfilter").addEventListener("click", function () {
+        document.getElementById("affiliations-input").selectedIndex = null;
+        const cols = document.querySelectorAll(".navigation-tiles .col");
+        for (col of cols) {
+            const colCategories = col.getAttribute("data-affiliations").split(" ");
+            col.classList.remove("d-none");
         }
-      }
     });
-  document.getElementById("clearfilter").addEventListener("click", function () {
-    document.getElementById("affiliations-input").selectedIndex = null;
-    const cols = document.querySelectorAll(".col");
-      for (col of cols) {
-        const colCategories = col.getAttribute("data-affiliations").split(" ");
-          col.style.display = "block";
-      }
-  });
 });
 
+/** 
+ * Search cards
+ */
+
+var buttonUp = () => {
+    const input = document.getElementById("title-search");
+    const cols = document.querySelectorAll(".navigation-tiles .col");
+    let filter = input.value.toLowerCase();
+    document.getElementById("affiliations-input").selectedIndex = null;
+    for (let i = 0; i < cols.length; i++) {
+        let body = cols[i].querySelector(".card-body");
+        let title = cols[i].querySelector(".card-header");
+        if (title.innerText.toLowerCase().indexOf(filter) > -1 || body.innerText.toLowerCase().indexOf(filter) > -1) {
+            cols[i].classList.remove("d-none");
+        } else {
+            cols[i].classList.add("d-none");
+        }
+    }
+}
