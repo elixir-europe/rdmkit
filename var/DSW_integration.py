d5064f891d25c37c7941e54ffb5c28f80cf0d8ae
bedroesb <bedro@psb.vib-ugent.be>
2022-01-25 15:30:15 +0100

    better logging

--- var/DSW_integration.py
+++ var/DSW_integration.py
@@ -21,15 +21,16 @@ def strip_end(text, suffix):
 
 # --------- Parsing DSW json ---------
 
-
+print(f"Reading out {dsw_json}")
 with open(dsw_json) as read_file:
     data = json.load(read_file)
-
+print("Done")
 
 parent_ids = {}
 titles_mapping = {}
 
 # Parsing DSW objects with RDMkit url mention
+print(f"Parsing DSW objects with RDMkit url mention")
 for package in data['packages']:
     if 'events' in package:
         for event in package['events']:
@@ -50,16 +51,17 @@ for package in data['packages']:
                 elif isinstance(event['title'], dict) and 'value' in event['title'] and event['title']['value']:
                     titles_mapping[event['entityUuid']
                                    ] = event['title']['value']
-
+print("Done")
 
 # --------- Updating markdown pages with DSW links ---------
 
-
+print(f"Updating markdown pages with DSW links")
 for subdir, dirs, files in os.walk(rootdir):
     for file_name in files:
         if os.path.splitext(file_name)[1] == '.md':
             filename_stripped = os.path.splitext(file_name)[0]
             with open(os.path.join(subdir, file_name), "r") as f:
+                print(f"Reading out {filename_stripped}")
                 contents = f.readlines()
                 frontmatter_start = False
                 frontmatter_end = 0
@@ -78,22 +80,28 @@ for subdir, dirs, files in os.walk(rootdir):
                         dsw_start = index
                     elif re.match(r'^[a-zA-Z]', line) and frontmatter_start and dsw_start:
                         dsw_end = index
-
                     if frontmatter_end:
+                        print(f"\tFrontmatter present from line 0 - {frontmatter_end}")
                         break
             if ( dsw_end and dsw_start ) or filename_stripped in parent_ids:
                 if  dsw_end and dsw_start:
+                    print(f"\tdsw block present from line {dsw_start} - {dsw_end}")
                     del contents[dsw_start:dsw_end]
+                    print(f"\tdsw block deleted")
                     frontmatter_end = frontmatter_end - (dsw_end - dsw_start)
-                
                 metadata = {}
                 if filename_stripped in parent_ids:
+                    print(f"\tMaking new frontmatter block")
                     dsw_info = []
                     for dsw_object in parent_ids[filename_stripped]:
                         dsw_info.append(
                             {'name': titles_mapping[dsw_object['parentUuid']], 'parentuuid': dsw_object['parentUuid']})
                     metadata['dsw'] = dsw_info
                 contents.insert(frontmatter_end, yaml.dump( metadata , default_flow_style=False))
+                print(f"\tNew frontmatter block inserted in content")
+                print(f"\tDumping content in markdown file {os.path.join(subdir, file_name)}")
                 with open(os.path.join(subdir, file_name), "w") as f:
                     contents = "".join(contents)
                     f.write(contents)
+                print(f"\tDone")
+print(f"DSW links successfully added to the markdown pages")
