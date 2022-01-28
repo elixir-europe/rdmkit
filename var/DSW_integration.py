a5cee8aca01a73d8d0c108c1fcf98a8ee739e936
bedroesb <bedro@psb.vib-ugent.be>
2022-01-28 12:29:41 +0100

    updated script

--- var/DSW_integration.py
+++ var/DSW_integration.py
@@ -1,57 +1,75 @@
-import sys
 import os
 import re
-import json
-import frontmatter
 from urllib.parse import urlparse
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+import collections
+from typing import Dict, List
 import yaml
 
 
 # --------- Variables ---------
 rootdir = 'pages/'
-dsw_json = '/home/bedro/Downloads/dsw_root-rdmkit_2.3.7.json'
+DSWQuestion = collections.namedtuple('DSWQuestion', 'uuid, text')
+RDMKIT_PREFIX = 'https://rdmkit.elixir-europe.org'
 
 
-# --------- Some intresting functions ---------
+# --------- Functions ---------
 
 def strip_end(text, suffix):
     if suffix and text.endswith(suffix):
         return text[:-len(suffix)]
     return text
 
+def rdmkit_page_id(url: str) -> str:
+    parsed_url = os.path.basename(urlparse(url).path)
+    return strip_end(parsed_url, '.html')
+
+
+def client(url, payload):
+    """API object fetcher"""
+    print(f"POST request with url= {url} and json= {payload}")
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=15)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    r = session.post(url, json=payload)
+    if r.status_code == requests.codes.ok:
+        return r.json()
+    else:
+        r.raise_for_status()
+
+
+def fetch_rdmkit_dsw_links(endpoint: str, package: str) -> Dict[str, List[DSWQuestion]]:
+    km = client(f'{endpoint}/knowledge-models/preview', {'packageId': package, 'events': [], 'tagUuids': []})
+    print(f"Parsing DSW objects with {endpoint} mention")
+    links = {}  # type: dict[str, list[DSWQuestion]]
+    rdmkit_references = {
+        ref_id: ref for ref_id, ref in km['entities']['references'].items()
+        if ref.get('url', '').startswith(RDMKIT_PREFIX)
+    }
+    for question_id, question in km['entities']['questions'].items():
+        rdmkit_refs = (ref for ref in question['referenceUuids']
+                       if ref in rdmkit_references.keys())
+        for ref in rdmkit_refs:
+            page_id = rdmkit_page_id(rdmkit_references[ref]['url'])
+            if page_id not in links.keys():
+                links[page_id] = []
+            links[page_id].append(
+                DSWQuestion(question_id, question['title'])
+            )
+    print("Done")
+    return links
+
+
 # --------- Parsing DSW json ---------
+parent_ids = fetch_rdmkit_dsw_links(
+    endpoint='https://api-converge.ds-wizard.org',
+    package='dsw:root-rdmkit:latest',
+)
 
-print(f"Reading out {dsw_json}")
-with open(dsw_json) as read_file:
-    data = json.load(read_file)
-print("Done")
-
-parent_ids = {}
-titles_mapping = {}
-
-# Parsing DSW objects with RDMkit url mention
-print(f"Parsing DSW objects with RDMkit url mention")
-for package in data['packages']:
-    if 'events' in package:
-        for event in package['events']:
-            if package['kmId'] == 'root-rdmkit' and 'url' in event and 'https://rdmkit.elixir-europe.org/' in event['url']:
-                parsed_url = os.path.basename(urlparse(event['url']).path)
-                filename = strip_end(parsed_url, '.html')
-                event_info = {}
-                event_info['url'] = event['url']
-                event_info['parentUuid'] = event['parentUuid']
-                if filename in parent_ids:
-                    parent_ids[filename].append(event_info)
-                else:
-                    parent_ids[filename] = [event_info]
-
-            elif 'title' in event and 'entityUuid' in event:
-                if isinstance(event['title'], str):
-                    titles_mapping[event['entityUuid']] = event['title']
-                elif isinstance(event['title'], dict) and 'value' in event['title'] and event['title']['value']:
-                    titles_mapping[event['entityUuid']
-                                   ] = event['title']['value']
-print("Done")
 
 # --------- Updating markdown pages with DSW links ---------
 
@@ -93,9 +111,9 @@ for subdir, dirs, files in os.walk(rootdir):
                 if filename_stripped in parent_ids:
                     print(f"\tMaking new frontmatter block")
                     dsw_info = []
-                    for dsw_object in parent_ids[filename_stripped]:
+                    for question in parent_ids[filename_stripped]:
                         dsw_info.append(
-                            {'name': titles_mapping[dsw_object['parentUuid']], 'parentuuid': dsw_object['parentUuid']})
+                            {'name': question.text, 'parentuuid': question.uuid})
                     metadata['dsw'] = dsw_info
                 contents.insert(frontmatter_end, yaml.dump( metadata , default_flow_style=False))
                 print(f"\tNew frontmatter block inserted in content")
