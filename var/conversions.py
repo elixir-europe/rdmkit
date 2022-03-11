ca8581e7bc6ceda8885b31992247e6aa041edb92
Bert Droesbeke <44875756+bedroesb@users.noreply.github.com>
2022-03-11 12:39:44 +0100

    tess search improvements (#873)

--- var/conversions.py
+++ var/conversions.py
@@ -33,10 +33,12 @@ def process_args():
 
     return args
 
-def parse_acronym (query):
+
+def parse_acronym(query):
     m = re.match(r"(.*)\s\((.*)\)", query)
     if m:
         return {"fullname": m.group(1), "acronym": m.group(2)}
+
 
 def client(url):
     """API object fetcher"""
@@ -51,10 +53,15 @@ def client(url):
 
 
 def tess_available(query):
-    json_output = client(
-        f"https://tess.elixir-europe.org/materials?q={query}&page_number=1&page_size=30")
-    if len(json_output) > 0:
-        return True
+    acronym = parse_acronym(query)
+    def fetch_output(query):
+        return client(
+            f'https://tess.elixir-europe.org/materials?q="{query}"&page_number=1&page_size=30')
+    if len(fetch_output(query)) > 0:
+        return query
+    if acronym and len(fetch_output(acronym['fullname'])) > 0:
+        return acronym['fullname']
+
 
 def biotools_available(query):
     acronym = parse_acronym(query)
@@ -77,9 +84,11 @@ def biotools_available(query):
                     if query.strip().lower() in tool['name'].lower():
                         return tool['biotoolsID']
 
+
 def get_fairsharing_token(username, password):
     url = "https://api.fairsharing.org/users/sign_in"
-    payload="{\"user\": {\"login\":\"" + username + "\",\"password\":\"" + password + "\"} }"
+    payload = "{\"user\": {\"login\":\"" + username + \
+        "\",\"password\":\"" + password + "\"} }"
     headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
@@ -94,6 +103,7 @@ def get_fairsharing_token(username, password):
     except:
         print("Could not login into FAIRsharing")
 
+
 def fairsharing_available(query, token):
     url = "https://api.fairsharing.org/search/fairsharing_records"
 
@@ -104,7 +114,8 @@ def fairsharing_available(query, token):
     }
     payload = {'q': query}
     try:
-        response = requests.request("POST", url, headers=headers, params=payload)
+        response = requests.request(
+            "POST", url, headers=headers, params=payload)
         output = response.json()['data']
         if len(output) >= 1:
             for farisharing_obj in output:
@@ -113,14 +124,15 @@ def fairsharing_available(query, token):
     except:
         print("Could not connect to FAIRsharing")
 
+
 def remove_prefix(s, prefix):
     return s[s[:len(prefix)].index(prefix) + len(prefix):]
 
 # --------- Variables ---------
 
+
 table_path = "_data/main_tool_and_resource_list.csv"
 output_path = "_data/tool_and_resource_list.yml"
-related_pages_path = "_data/page_ids.yml"
 rootdir = 'pages/'
 allowed_registries = ['biotools', 'fairsharing', 'tess', 'fairsharing-coll']
 
@@ -137,11 +149,15 @@ for subdir, dirs, files in os.walk(rootdir):
                 metadata, content = frontmatter.parse(f.read())
             if 'page_id' in metadata.keys() and 'search_exclude' not in metadata.keys():
                 pages_metadata[metadata['page_id']] = {}
-                pages_metadata[metadata['page_id']]['title'] = metadata['title']
-                pages_metadata[metadata['page_id']]['type'] = remove_prefix( subdir ,'pages/').replace("_", " ").capitalize()
-                pages_metadata[metadata['page_id']]['url'] = os.path.splitext(file_name)[0] + ".html"
+                pages_metadata[metadata['page_id']
+                               ]['title'] = metadata['title']
+                pages_metadata[metadata['page_id']]['type'] = remove_prefix(
+                    subdir, 'pages/').replace("_", " ").capitalize()
+                pages_metadata[metadata['page_id']]['url'] = os.path.splitext(file_name)[
+                    0] + ".html"
                 if 'description' in metadata:
-                    pages_metadata[metadata['page_id']]['description'] = metadata['description']
+                    pages_metadata[metadata['page_id']
+                                   ]['description'] = metadata['description']
 
 print(f"----> Allowed related_pages: {', '.join(pages_metadata.keys())}.")
 
@@ -162,30 +178,35 @@ with open(table_path, 'r') as read_obj:
             tool = {}
             tool_name = row[0]
             for col_index, cell in enumerate(row):
-                if header[col_index] == 'related_pages' and cell:# Only include keys if there are values:
+                # Only include keys if there are values:
+                if header[col_index] == 'related_pages' and cell:
                     output = re.split(', |,', cell)
                     for tag in output:
                         if tag not in pages_metadata.keys():
-                            print(f'ERROR: The table contains the tag "{tag}" in row {row_index} which is not allowed.\n-> Check if the tag you are using is declared in the metadata of one of the pages using the "page_id" attribute.')
+                            print(
+                                f'ERROR: The table contains the tag "{tag}" in row {row_index} which is not allowed.\n-> Check if the tag you are using is declared in the metadata of one of the pages using the "page_id" attribute.')
                             sys.exit(
                                 f'The table contains the tag "{tag}" in row {row_index} which is not allowed.\n-> Check if the tag you are using is declared in the metadata of one of the pages using the "page_id" attribute.')
-                elif header[col_index] == 'country' and cell:# Only include keys if there are values:
+                # Only include keys if there are values:
+                elif header[col_index] == 'country' and cell:
                     output = re.split(', |,', cell)
                 elif header[col_index] == 'registry':
                     output = {}
-                    if cell:# Only include keys if there are values
+                    if cell:  # Only include keys if there are values
                         for registry in re.split(', |,', cell):
                             reg, identifier = re.split(':|: ', registry)
                             if reg in allowed_registries:
                                 output[reg] = identifier
                             else:
-                                print(f'ERROR: The table contains the registry "{reg}" in row {row_index} which is not allowed.\n' + f"Allowed registries are {', '.join(allowed_registries)}.\n")
+                                print(
+                                    f'ERROR: The table contains the registry "{reg}" in row {row_index} which is not allowed.\n' + f"Allowed registries are {', '.join(allowed_registries)}.\n")
                                 sys.exit(
                                     f'The table contains the registry "{reg}" in row {row_index} which is not allowed.\n' + f"Allowed registries are {', '.join(allowed_registries)}.\n")
                     if args.reg:
                         if "tess" not in output:
-                            if tess_available(tool_name):
-                                output["tess"] = tool_name
+                            check_tess = tess_available(tool_name)
+                            if check_tess:
+                                output["tess"] = check_tess
                         elif output["tess"] == "NA":
                             del output["tess"]
                         if "biotools" not in output:
@@ -196,7 +217,8 @@ with open(table_path, 'r') as read_obj:
                             del output["biotools"]
                         if "fairsharing" not in output:
                             if len(tool_name) > 4:
-                                check_fairsharing = fairsharing_available(tool_name, fairsharing_token)
+                                check_fairsharing = fairsharing_available(
+                                    tool_name, fairsharing_token)
                                 if check_fairsharing:
                                     output["fairsharing"] = check_fairsharing
                         elif output["fairsharing"] == "NA":
