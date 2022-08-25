c22072aa01fa2055872318f92b72e7ef503cdb29
Bert Droesbeke <44875756+bedroesb@users.noreply.github.com>
2022-08-25 15:38:29 +0200

    fix TeSS API url (#969)

--- var/conversions.py
+++ var/conversions.py
@@ -56,10 +56,10 @@ def tess_available(query):
     acronym = parse_acronym(query)
     def fetch_output(query):
         return client(
-            f'https://tess.elixir-europe.org/materials?q="{query}"&page_number=1&page_size=30')
-    if len(fetch_output(query)) > 0:
+            f'https://tess.elixir-europe.org/materials.json_api?q="{query}"&page_number=1&page_size=30')
+    if len(fetch_output(query)['data']) > 0:
         return query
-    if acronym and len(fetch_output(acronym['fullname'])) > 0:
+    if acronym and len(fetch_output(acronym['fullname'])['data']) > 0:
         return acronym['fullname']
 
 
